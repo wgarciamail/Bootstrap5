7df71d0084c1cd740a8c9e7cb05c24a8932701fc
William Garcia <wgarcia@tradercomusa.com>
2023-04-05 13:48:31 -0400

    refacts: Elimina código que no utilizo.

--- pruebas/modal/pruebaModal.js
+++ pruebas/modal/pruebaModal.js
@@ -10,27 +10,3 @@ function sendNumber(){
     successInformation.classList.remove("hidden")
     numberData.classList.add("hidden");
 }
-
-function takePhoneNumber(){
-    console.log("recibiendo informacion desde la ventana modal");
-    let phoneNumber = document.getElementById("phoneNumber").value;
-    let validationNumber = document.getElementById("validationNumber");
-    let numberData = document.getElementById("numberData");
-    let exampleModalLabel = document.getElementById("exampleModalLabel");
-    if (phoneNumber == null || phoneNumber == ""){
-        phoneNumber = Math.floor(Math.random() * 5000) + 1;
-    }
-
-    document.getElementById("phoneNumeberRecived").innerHTML = phoneNumber;
-    validationNumber.classList.remove("hidden");
-    numberData.classList.add("hidden");
-    exampleModalLabel.innerHTML = "Ingrese su código de validación."
-
-}
-/* 
-function raiseModal(){
-    var myModal = new bootstrap.Modal(document.getElementById("verifyCodeModal"), {});
-    document.onclick = function () {
-    myModal.show();
-    };
-} */
