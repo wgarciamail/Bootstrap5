6a026c7f4cbe4acd0b3b2a008e7be867ef992577
William Garcia <wgarcia@tradercomusa.com>
2023-04-05 13:52:57 -0400

    refacts : format en los archivos.

--- pruebas/modal/pruebaModal.js
+++ pruebas/modal/pruebaModal.js
@@ -1,10 +1,10 @@
-function sendAddress(){
+function sendAddress() {
     let addressInformation = document.getElementById("addressData");
     let numberData = document.getElementById("numberData");
     numberData.classList.remove("hidden");
     addressInformation.classList.add("hidden");
 }
-function sendNumber(){
+function sendNumber() {
     let numberData = document.getElementById("numberData");
     let successInformation = document.getElementById("success-information");
     successInformation.classList.remove("hidden")
